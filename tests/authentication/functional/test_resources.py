d6d7c23622acbce6861bc619543c0d4afc43ed0d
docoprusta <gyore.soma@gmail.com>
2017-10-26 23:12:24 +0200

    functional tests and travis.yml modified

--- tests/authentication/functional/test_resources.py
+++ tests/authentication/functional/test_resources.py
@@ -11,25 +11,18 @@ from app.authentication.controller import UserController
 
 class TestAuthenticationResources(object):
 
-    pid = 0
-    jwt = ""
-    user_controller = UserController()
+    PORT = 5001
 
     @classmethod
     def setup_class(cls):
-        command = shlex.split("gunicorn --bind '127.0.0.1:5002' wsgi:flask_app")
-        process = subprocess.Popen(command)
-        cls.pid = process.pid
-
         test_user_dict = {
             "username": "testuser",
             "password": "testpw"
         }
 
-        cls.user_controller.delete_every_user()
-        cls.user_controller.create_user(test_user_dict)
+        requests.post('http://127.0.0.1:{}/register'.format(cls.PORT), json=test_user_dict)
 
-        login_response = requests.post('http://127.0.0.1:5002/login', json=test_user_dict)
+        login_response = requests.post('http://127.0.0.1:{}/login'.format(cls.PORT), json=test_user_dict)
 
         jwt = login_response.json()['accessToken']
 
@@ -43,126 +36,108 @@ class TestAuthenticationResources(object):
             "Authorization": "Bearer {}".format(refresh_jwt)
         }
 
-    @classmethod
-    def teardown_class(cls):
-        os.kill(cls.pid, signal.SIGTERM)
-
     def test_register_and_login_ok(self):
-        self.user_controller.delete_every_user()
         test_user_dict = {
             "username": "testuser2",
             "password": "testpw2"
         }
 
-        requests.post('http://127.0.0.1:5002/register', json=test_user_dict)
+        requests.post('http://127.0.0.1:{}/register'.format(self.PORT), json=test_user_dict)
 
-        login_response = requests.post('http://127.0.0.1:5002/login', json=test_user_dict)
+        login_response = requests.post('http://127.0.0.1:{}/login'.format(self.PORT), json=test_user_dict)
 
         assert login_response.status_code == 200
 
     def test_refresh_token_unauthorized(self):
-        refresh_token_response = requests.get('http://127.0.0.1:5002/refresh-token')
+        refresh_token_response = requests.get('http://127.0.0.1:{}/refresh-token'.format(self.PORT))
 
         assert refresh_token_response.status_code == 401
 
     def test_refresh_token_unauthorized_with_access_token(self):
-        refresh_token_response = requests.get('http://127.0.0.1:5002/refresh-token', headers=self.headers)
+        refresh_token_response = requests.get('http://127.0.0.1:{}/refresh-token'.format(self.PORT), headers=self.headers)
         assert refresh_token_response.status_code == 422
 
     def test_refresh_token_ok(self):
-        refresh_token_response = requests.get('http://127.0.0.1:5002/refresh-token', headers=self.refresh_headers)
+        refresh_token_response = requests.get('http://127.0.0.1:{}/refresh-token'.format(self.PORT), headers=self.refresh_headers)
 
         assert refresh_token_response.status_code == 200
 
     def test_register_user_already_exist(self):
-        self.user_controller.delete_every_user()
         test_user_dict = {
             "username": "testuser3",
             "password": "testpw3"
         }
 
-        requests.post('http://127.0.0.1:5002/register', json=test_user_dict)
-        register_response = requests.post('http://127.0.0.1:5002/register', json=test_user_dict)
+        requests.post('http://127.0.0.1:{}/register'.format(self.PORT), json=test_user_dict)
+        register_response = requests.post('http://127.0.0.1:{}/register'.format(self.PORT), json=test_user_dict)
 
         assert register_response.status_code == 409
 
     def test_login_unauthorized(self):
-        self.user_controller.delete_every_user()
         test_user_dict = {
             "username": "testuser4",
             "password": "testpw4"
         }
 
-        requests.post('http://127.0.0.1:5002/register', json=test_user_dict)
+        requests.post('http://127.0.0.1:{}/register'.format(self.PORT), json=test_user_dict)
 
         login_user_dict = {
             "username": "testuser4",
             "password": "somethingelse"
         }
 
-        login_response = requests.post('http://127.0.0.1:5002/login', json=login_user_dict)
+        login_response = requests.post('http://127.0.0.1:{}/login'.format(self.PORT), json=login_user_dict)
 
         assert login_response.status_code == 401
 
     def test_delete_user_unauthorized(self):
-        delete_response = requests.delete('http://127.0.0.1:5002/users?username=testuser')
+        delete_response = requests.delete('http://127.0.0.1:{}/users?username=testuser'.format(self.PORT))
         assert delete_response.status_code == 401
 
     def test_delete_user_user_does_not_exist(self):
-        delete_response = requests.delete('http://127.0.0.1:5002/users?username=nonexistinguser', headers=self.headers)
+        delete_response = requests.delete('http://127.0.0.1:{}/users?username=nonexistinguser'.format(self.PORT), headers=self.headers)
         assert delete_response.status_code == 404
 
     def test_delete_user_user_ok(self):
-        self.user_controller.delete_every_user()
         test_user_dict = {
             "username": "testuser5",
             "password": "testpw5"
         }
 
-        requests.post('http://127.0.0.1:5002/register', json=test_user_dict)
+        requests.post('http://127.0.0.1:{}/register'.format(self.PORT), json=test_user_dict)
 
-        delete_response = requests.delete('http://127.0.0.1:5002/users?username=testuser5', headers=self.headers)
-        assert delete_response.status_code == 200
-
-    def test_get_user_unauthorized(self):
-        delete_response = requests.get('http://127.0.0.1:5002/users?api-key=someapikey')
-        assert delete_response.status_code == 401
-
-    def test_get_user_user_does_not_exist(self):
-        delete_response = requests.get('http://127.0.0.1:5002/users?api-key=someapikey', headers=self.headers)
-        assert delete_response.status_code == 404
-
-    def test_get_user_user_ok(self):
-        self.user_controller.delete_every_user()
-        test_user_dict = {
-            "username": "testuser6",
-            "password": "testpw6"
-        }
-
-        requests.post('http://127.0.0.1:5002/register', json=test_user_dict)
-
-        fetched_user = self.user_controller.get_user_by_username('testuser6')
-
-        delete_response = requests.get(
-            'http://127.0.0.1:5002/users?api-key={}'.format(fetched_user.api_key),
+        delete_response = requests.delete(
+            'http://127.0.0.1:{}/users?username=testuser5'.format(self.PORT),
             headers=self.headers
         )
         assert delete_response.status_code == 200
 
+    def test_get_user_unauthorized(self):
+        delete_response = requests.get('http://127.0.0.1:{}/users?api-key=someapikey'.format(self.PORT))
+        assert delete_response.status_code == 401
+
+    def test_get_user_user_does_not_exist(self):
+        delete_response = requests.get(
+            'http://127.0.0.1:{}/users?api-key=someapikey'.format(self.PORT), headers=self.headers
+        )
+        assert delete_response.status_code == 404
+
     def test_logout_access_unauthorized(self):
-        response_logout_access = requests.delete('http://127.0.0.1:5002/logout-access')
+        response_logout_access = requests.delete('http://127.0.0.1:{}/logout-access'.format(self.PORT))
         assert response_logout_access.status_code == 401
 
     def test_logout_access_ok(self):
-        response_logout_access = requests.delete('http://127.0.0.1:5002/logout-access', headers=self.headers)
+        response_logout_access = requests.delete(
+            'http://127.0.0.1:{}/logout-access'.format(self.PORT),
+            headers=self.headers)
         assert response_logout_access.status_code == 200
 
     def test_logout_refresh_unauthorized(self):
-        response_logout_access = requests.delete('http://127.0.0.1:5002/logout-refresh')
+        response_logout_access = requests.delete('http://127.0.0.1:{}/logout-refresh'.format(self.PORT))
         assert response_logout_access.status_code == 401
 
     def test_logout_resfresh_ok(self):
-        response_logout_access = requests.delete('http://127.0.0.1:5002/logout-refresh', headers=self.refresh_headers)
+        response_logout_access = requests.delete(
+            'http://127.0.0.1:{}/logout-refresh'.format(self.PORT), headers=self.refresh_headers
+        )
         assert response_logout_access.status_code == 200
-
-
